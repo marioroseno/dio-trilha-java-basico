e61d3e7120126857c3d9a9b422c462fbe5c19833
devmaroba <devmaroba@gmail.com>
2024-05-06 20:53:38 -0300

    OperadoresJava

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,80 @@
+public class Operadores {
+    public static void main(String[] args){
+       
+       /*Aula operadores - 2
+
+       String nomeCompleto = "Mário" + "Roseno";
+       System.out.println(nomeCompleto);
+
+        
+       Aula operadores - 3
+
+       String concatenacao = "?";
+
+       concatenacao = 1 + 1 + 1 + "1";
+       System.out.println(concatenacao);
+
+        #Aula - 4
+
+       int numero = 5;
+
+       System.out.println(++ numero);
+       System.out.println(numero);
+
+       // Com Booleanos
+
+       boolean variavel = true;
+
+       System.out.println(!variavel);
+       variavel = !variavel;
+
+       System.out.println(variavel);*/
+
+       /*#Aula - 5 (Operador Ternario)
+
+       int a, b;
+
+       a = 5;
+       b = 6;
+
+       String resultado = "";
+
+       if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "Falso";
+        
+        System.out.println(resultado);
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);*/
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual ao numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente do numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUM é maior que numeroDois? " + simNao);
+
+        simNao = numero1 < numero2;
+
+        System.out.println("numero1 é menor que numero2? " + simNao);
+
+
+
+
+
+
+
+
+    }
+}
